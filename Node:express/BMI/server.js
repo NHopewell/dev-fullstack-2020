8ee107c110f2b5daeef7d4b3a533e3b829891c80
Nick Hopewell <nicholashopewell@gmail.com>
2020-04-20 19:10:48 -0400

    adds bmi files

---
+++ Node:express/BMI/server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded( { extended: true} ));
+
+// send bmi html file at root route
+const currentDir = __dirname;
+app.get("/", (req, res) => res.sendFile( `${currentDir}/bmiCalculator.html`));
+
+// post to calculate BMI
+app.post("/bmiCalculator", (req, res) => {
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+
+    var bmi = Math.round(weight / (Math.pow(height, 2)));
+    
+    if (bmi > 24.9) {
+        end = ", so you are overweight";
+    } else if (bmi > 18.5) {
+        end = ", so you are normal weight";
+    } else {
+        end = ", so you are underweight";
+    }
+
+    msg = `Your BMI is ${bmi}${end}.`
+    
+    res.send(msg);
+})
+
+app.listen(2000);
