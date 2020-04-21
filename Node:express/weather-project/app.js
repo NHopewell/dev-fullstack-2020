c95454e822f991583c84a26a5ff5bceb26a7f6d9
Nick Hopewell <nicholashopewell@gmail.com>
2020-04-21 15:30:14 -0400

    updates weather app

--- Node:express/weather-project/app.js
+++ Node:express/weather-project/app.js
@@ -18,14 +18,27 @@ app.get("/", (req, res) => {
         // parse json response
         response.on('data', (data) => {
             const weatherData = JSON.parse(data);
+            console.log(weatherData);
             // temp and description of weather
             const temp = weatherData.main.temp;
             const description = weatherData.weather[0].description;
-            console.log(temp);
-            console.log(description);
+            const weatherIcon = weatherData.weather[0].icon;
+
+            // write the two data strings (remember can only call res.send() once)
+            const tempStr = `<h1>The temperature in London is ${temp} degrees celcius.</h1>`;
+            const descStr = `<p>Current weather description: ${description}</p>`
+            // icon of current weather sent as img
+            const icon = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+            const iconStr = `<img src='${icon}' alt='current weather icon' height='90' width='90'>`
+
+            // write data and send
+            res.write(`${tempStr}`);
+            res.write(`${descStr}`);
+            res.write(`${iconStr}`);
+            res.send()
         })
     // must pass absolute path to the server
-    res.sendFile(`${cwd}/index.html`);
+    // res.sendFile(`${cwd}/index.html`);
     })
 })
 
