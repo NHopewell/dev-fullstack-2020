1732766fddfae03cf5c45dea56c4d22169934798
Nick Hopewell <nicholashopewell@gmail.com>
2020-04-23 19:05:11 -0400

    updates app.js

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,6 +1,27 @@
 
 const express = require("express");
-const app = epxress();
+const app = express();
+
+// set apps view enjine to ejs
+app.set('view engine', 'ejs');
 
 const bodyParser = require("body-parser");
-app.use(bodyParser, )
+
+const cwd = __dirname;
+const port = 3000;
+
+
+app.get("/", (req, res) => {
+    
+    var today = new Date();
+    var currentDay = today.getDate()
+    
+    if ([6, 7].includes(today)) {
+        res.write("<h1>Yay, its the weekend</h1>");
+    } else {
+        res.sendFile(`${cwd}/index.html`);
+    }
+})
+
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
