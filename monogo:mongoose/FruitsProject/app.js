2ad1696e0db9b152df9a92003dc6ce306614e38c
Nick Hopewell <nicholashopewell@gmail.com>
2020-05-09 15:50:20 -0400

    updates mongo driver code to retrieve example rows

--- monogo:mongoose/FruitsProject/app.js
+++ monogo:mongoose/FruitsProject/app.js
@@ -10,8 +10,8 @@ const dbName = 'fruitsDB';
 // Create a new MongoClient
 const client = new MongoClient(url, { useNewUrlParser: true });
 
-// Use connect method to connect to the Server
-client.connect(function(err, client) {
+// Use connect method to connect to the Server and insert then retrieve
+client.connect( function(err, client) {
   assert.equal(null, err);
   console.log("Connected correctly to server");
 
@@ -22,25 +22,34 @@ client.connect(function(err, client) {
     // Insert multiple documents
     collection.insertMany([
         {
-            name: "Apple",
-            score: 8,
-            review: "Great fruit"
-        },
-        {
-            name: "Orange",
+            name: "Grape",
             score: 6,
-            review: "Kinda sour"
+            review: "meh"
         },
         {
-            name: "Banana",
-            score: 9,
-            review: "Great stuff!"
+            name: "Pear",
+            score: 10,
+            review: "Perfect"
+        },
+        {
+            name: "Kiwi",
+            score: 4,
+            review: "Bad"
         }
     ], function(err, result) {
       // assert no errors and that 3 docs got inserted
       assert.equal(null, err);
       assert.equal(3, result.insertedCount);
-
-      client.close();
     });
+
+    // Get first 5 records back from db as an array of js objects
+    collection.find({}).limit(5).toArray(function(err, fruits) {
+        assert.equal(null, err);
+        assert.equal(5, fruits.length);
+
+        console.log(fruits);
+
+        // close connection
+        client.close();
+        });
 });
