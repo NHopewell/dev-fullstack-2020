aabe21046f57dcf6bc241af2ce7c36ee6a66a805
Nick Hopewell <nicholashopewell@gmail.com>
2020-05-13 20:49:18 -0400

    updates app.js

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -26,12 +26,35 @@ const itemSchema = {
 // mongoose model
 const Item = mongoose.model("Item", itemSchema);
 
+// make some items
+const item1 = new Item ({
+  name: "Welcome to your todo list!"
+});
 
+const item2 = new Item ({
+  name: "Hit the + button to add a new item."
+});
+
+const item3 = new Item ({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+//insert documents into db collection
+Item.insertMany(defaultItems, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully inserted items");
+  }
+});
+
+
+// get and post routes
 app.get("/", function(req, res) {
 
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
+  res.render("list", {listTitle: "Today", newListItems: items});
 
 });
 
